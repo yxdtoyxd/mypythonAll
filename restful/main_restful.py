8b8197cfc06d6ac84c3068d4d8b3c6c68223a016
yaoxudong <yaoxudong@shouqianba.com>
2023-08-02 14:29:57 +0800

    no message

--- restful/main_restful.py
+++ restful/main_restful.py
@@ -7,10 +7,10 @@ from sub2_restful import sub2_restful_api
 
 app = Flask(__name__)
 
-# 蓝图
+# blueprint
 app.register_blueprint(sub1_restful_api)
 app.register_blueprint(sub2_restful_api)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8001, debug=True)
+    app.run(host='0.0.0.0', port=8001, debug=True)
